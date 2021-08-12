f49638917c8eeebc4158a7cf7b84dac7a76cbd40
dilsha umayangi <dilshaumayangi213@gmail.com>
2021-08-12 15:16:53 +0530

    added Authorization to cms

---
+++ Student-Reg/src/main/java/com/LMS/Student/Reg/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.LMS.Student.Reg.config;
+
+
+import com.LMS.Student.Reg.jwt.AuthTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+
+
+}
